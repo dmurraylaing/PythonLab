aba6876e1af428504a819936b8a779f9defb16b6
Murray Laing <murray.laing@aspentech.com>
2017-10-22 12:04:25 -0400

    Copy over initial draft code.

--- SpriteViewer/SpriteViewer.py
+++ SpriteViewer/SpriteViewer.py
@@ -1,1 +1,36 @@
+import os
+from sprite import Sprite
 
+def list_sprite_files(directory):
+    """Get the list of sprite files from the current directory"""
+    files = os.listdir(directory)
+
+    sprite_files = []
+    for file in files:
+        if file.endswith(".sprite"):
+           sprite_files.append(directory + "/" + file)
+
+    return sprite_files
+
+    #return [x for x in os.listdir(directory) if x.endswith(".sprite")]
+
+def pick_sprite_file(filelist):
+    """Function to show list to user and get them to pick one"""
+    return filelist[0]
+
+# Get the list of sprite files
+sprite_files = list_sprite_files("Sprites")
+
+# Display list to user and get them to pick one
+selected_file = pick_sprite_file(sprite_files)
+
+print("selected file " + selected_file)
+
+# Create a sprite object to hold the data
+sprite = Sprite()
+
+# Load the sprite data
+sprite.load_from_file(selected_file)
+
+# Display the sprite
+sprite.draw()
---
+++ SpriteViewer/sprite.py
@@ -0,0 +1,75 @@
+import turtle
+
+class Sprite:
+    """Class to hold the data for a sprite"""
+
+    def __init__(self, **kwargs):
+
+        self._color_map = dict()
+        self._color_map['B']='black'
+        self._color_map[' ']='white'
+        return super().__init__(**kwargs)
+    
+    def load_from_file(self, file):
+
+        """Read the content of a file into the sprite"""
+        self._file = file
+
+
+        self._lines = []
+
+        with open(file, "rtU") as f:
+
+            for textline in f:
+                line = []
+                # Now you have one line of text in the variable "line" and can
+                # iterate over its characters like so:
+                for ch in textline:
+                    if ch != '\n':
+                        line.append(self._color_map[ch])
+
+                self._lines.append(line)
+
+
+    def draw(self):
+        """Draw the sprite"""
+        print("Drawing sprite from " + self._file)
+
+        self._draw_start()
+
+        lineno = 0
+        for line in self._lines:
+            for pixel in line:
+                self._draw_pixel(pixel)
+
+            lineno = lineno + 1
+            self._draw_start_of_line(lineno)
+
+    def _draw_start_of_line(self, lineno):
+        turtle.penup()
+        turtle.home()
+        turtle.right(90)
+        turtle.forward(15*lineno)
+        turtle.left(90)
+        turtle.pendown()
+
+
+    def _draw_start(self):
+        pass
+
+    def _draw_pixel(self, pixel):
+        print("Drawing pixel " + pixel)
+        turtle.pen(fillcolor=pixel, pencolor=pixel, pensize=1)
+        turtle.begin_fill()
+        turtle.forward(15)
+        turtle.left(90)
+        turtle.forward(15)
+        turtle.left(90)
+        turtle.forward(15)
+        turtle.left(90)
+        turtle.forward(15)
+        turtle.left(90)
+        turtle.end_fill()
+        turtle.penup()
+        turtle.forward(15)
+        turtle.pendown()
